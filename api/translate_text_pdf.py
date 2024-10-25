aee7e0ca612b478a4514a42b6347f541ac888b38
Vladimir Sobetskii <Sobetskiy.81@gmail.com>
2024-10-25 19:21:05 +0300

    fix переводчика

--- api/translate_text_pdf.py
+++ api/translate_text_pdf.py
@@ -7,17 +7,40 @@ from api.s3_client import s3_client, AWS_BUCKET_NAME
 from docx import Document
 
 
+def split_text(text, max_length=1000):
+    """Разделяет текст на части, не превышающие max_length."""
+    words = text.split()
+    current_part = []
+    current_length = 0
+
+    for word in words:
+        if current_length + len(word) + 1 > max_length:
+            yield ' '.join(current_part)
+            current_part = [word]
+            current_length = len(word)
+        else:
+            current_part.append(word)
+            current_length += len(word) + 1
+
+    if current_part:
+        yield ' '.join(current_part)
+
+
 async def detect_and_transl_text_pdf(file_name):
     file_obj = s3_client.get_object(Bucket=AWS_BUCKET_NAME, Key=file_name)
     file_content = file_obj['Body'].read()
     with pymupdf.open(stream=file_content) as doc:
         detected_text = chr(12).join([page.get_text() for page in doc])
-    trans_text = ts.translate_text(query_text=detected_text,
-                                   translator='bing',
-                                   from_language='auto',
-                                   to_language='en',
-                                   )
-    return trans_text
+    translated_parts = []
+    for part in split_text(detected_text):
+        trans_text = ts.translate_text(query_text=part,
+                                       translator='bing',
+                                       from_language='auto',
+                                       to_language='en')
+        translated_parts.append(trans_text)
+
+    final_translated_text = '\n'.join(translated_parts)
+    return final_translated_text
 
 
 async def create_pdf(query_text, file_name):
--- api/translate_text_word.py
+++ api/translate_text_word.py
@@ -9,24 +9,49 @@ from api.s3_client import s3_client, AWS_BUCKET_NAME
 from docx import Document
 
 
+def split_text(text, max_length=1000):
+    """Разделяет текст на части, не превышающие max_length."""
+    words = text.split()
+    current_part = []
+    current_length = 0
+
+    for word in words:
+        if current_length + len(word) + 1 > max_length:
+            yield ' '.join(current_part)
+            current_part = [word]
+            current_length = len(word)
+        else:
+            current_part.append(word)
+            current_length += len(word) + 1
+
+    if current_part:
+        yield ' '.join(current_part)
+
+
 async def detect_and_transl_text_word(file_name: str):
     file_obj = s3_client.get_object(Bucket=AWS_BUCKET_NAME, Key=file_name)
     with tempfile.NamedTemporaryFile(delete=False, suffix='.docx') as temp_file:
         temp_file.write(file_obj['Body'].read())
         temp_file_path = temp_file.name
+
     document = Document(temp_file_path)
     detected_text = '\n'.join([para.text for para in document.paragraphs if para.text.strip()])
     os.remove(temp_file_path)
-    trans_text = ts.translate_text(query_text=detected_text,
-                                   translator='bing',
-                                   from_language='auto',
-                                   to_language='en',
-                                   )
-    return trans_text
+
+    translated_parts = []
+    for part in split_text(detected_text):
+        trans_text = ts.translate_text(query_text=part,
+                                       translator='bing',
+                                       from_language='auto',
+                                       to_language='en')
+        translated_parts.append(trans_text)
+
+    final_translated_text = '\n'.join(translated_parts)
+    return final_translated_text
 
 
 async def create_word(query_text: str, file_name: str):
-    translated_word_filename = f"trans_{str(file_name)[:-4]}.docx"
+    translated_word_filename = f"trans_{str(file_name)[:-5]}.docx"
     word_buffer = io.BytesIO()
 
     word_writer = Document()
